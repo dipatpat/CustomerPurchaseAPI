74002a954ac4f5d7218c8b1948b109c0116f2d4d
Patrycja Szpakowska <patrycja.szpakowska@gmail.com>
2025-06-14 07:06:27 +0200

    Add swagger

--- CustomerPurchase/Program.cs
+++ CustomerPurchase/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
 namespace CustomerPurchase;
 
 public class Program
@@ -5,14 +8,49 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-
+        
+        
         // Add services to the container.
         builder.Services.AddAuthorization();
 
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Event API",
+                Version = "v1",
+                Description = "API for Managing Customer Purchases",
+                Contact = new OpenApiContact
+                {
+                    Name = "API Support",
+                    Email = "support@example.com",
+                    Url = new Uri("https://example.com/support")
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "MIT License",
+                    Url = new Uri("https://opensource.org/licenses/MIT")
+                }
+            });
+        });
+        
 
         var app = builder.Build();
+        
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Customer Purchases API v1");
+                c.DocExpansion(DocExpansion.List);
+                c.DefaultModelsExpandDepth(0);
+                c.DisplayRequestDuration();
+                c.EnableFilter();
+            });
+        }
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
